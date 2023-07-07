f04dabf4d8096987ce4f25adb0b601d85774d3f0
ateresasilva <ateresa.s22@gmail.com>
2023-07-07 16:50:16 +0100

    Changed some classes and methods and added messages;

--- src/main/java/academy/mindswap/p1g2/casino/server/games/blackjack/BlackJack.java
+++ src/main/java/academy/mindswap/p1g2/casino/server/games/blackjack/BlackJack.java
@@ -1,5 +1,8 @@
 package academy.mindswap.p1g2.casino.server.games.blackjack;
 
+import java.util.List;
+import java.util.Random;
+
 /** Rules:
  * 52 cards;
  * cards from 2 to 10 has value of 10;
@@ -14,74 +17,73 @@ package academy.mindswap.p1g2.casino.server.games.blackjack;
  * "double down" - increase the initial bet by 100% and take exactly one more card;
  * "surrender" - lose half the bet and end the hand immediately;
  */
+public class BlackJack {
+    private static int HIGH_SCORE = 21;
+    private static boolean isGameOver;
+    private int cardValue;
+    private int credits;
+    public Player player = new Player("username");
+    public Player dealer = new Player("username2");
+    public Deck deck = new Deck();
 
-    public class BlackJack {
-        private static final int HIGH_SCORE = 21;
-        private static boolean isGameOver;
-        private int cardValue;
+    public void play(){
+        System.out.println(Messages.WELCOME);
+        System.out.println(Messages.START);
+        System.out.println(Messages.PLAYER);
+        player.getPlayerName();
+        System.out.println(Messages.DEALER);
+        dealer.getPlayerName();
 
+    }
 
-        public static void main(String[] args) {
-            Deck deck = new Deck();
-            deck.initializeDeck();
-            deck.shuffle();
-            Deck.Card card1 = deck.drawCard();
-            System.out.println(card1);
-
-            /*System.out.println("Welcome to our game!!!");
-            Player dealer = new Player("John");
-            System.out.println(dealer.getPlayerName() + " is this round dealer.");
-            Player player = new Player("Mary");
-            System.out.println(dealer.getPlayerName() + " is playing against " + player.getPlayerName() + "!");
-            System.out.println("Let's start!");
-            BlackJack bj = new BlackJack();
-            bj.playGame(player, dealer);*/
-        }
-
-       /* public void playGame(Player player, Player dealer) {
-            while (!isGameOver) {
-                if (!doTurn(player)) {
-                    printWinner(dealer);
-                    return;
-                }
-                if (!doTurn(dealer)) {
-                    printWinner(player);
-                    return;
-                }
-                Player winner = theWinnerIs(player, dealer);
-                if (winner != null) {
-                    printWinner(winner);
-                    return;
-                }
-            }
-        } */
-
-        private void printWinner(Player player) {
-            System.out.printf("%s won the game!!!", player.getPlayerName());
-        }
-
-        /*public boolean doTurn(Player player) {
-            int cardValue = generateCard();
-            player.score(cardValue);
-            System.out.printf("Player: %s take %d card and now has %d points %n", player.getPlayerName(), cardValue, player.score(cardValue));
-
-            if(isBurst(player)){
-                System.out.printf("Player %s burst %n",player.getPlayerName());
-                isGameOver = true;
-            }
-            return false;
-        }*/
-
-        //
-        private static Player theWinnerIs(Player player, Player dealer){
-            if(player.getScore() == 21 && dealer.getScore() == 21){
-                return dealer;
-            }
-            return player.getScore() == 21 ? player : dealer.getScore() == 21 ? dealer : null;
-        }
-
-        private static boolean isBurst(Player player) {
-            return player.score(player.cardValue) > HIGH_SCORE;
+    private void hit(){
+        deck.generateDeck();
+        for(int cardValue : deck.cards){
+            System.out.println(cardValue);
         }
     }
+    public void playGame(Player player, Player dealer) {
+        while (!isGameOver) {
+            if (!doTurn(player)) {
+                printWinner(dealer);
+                return;
+            }
+            if (!doTurn(dealer)) {
+                printWinner(player);
+                return;
+            }
+            Player winner = theWinnerIs(player, dealer);
+            if (winner != null) {
+                printWinner(winner);
+                return;
+            }
+        }
+    }
+
+    private void printWinner(Player player) {
+        System.out.printf("%s won the game!!!", player.getPlayerName());
+    }
+
+    public boolean doTurn(Player player) {
+        player.score(cardValue);
+        System.out.printf("Player: %s take %d card and now has %d points %n", player.getPlayerName(), cardValue, player.score(cardValue));
+
+        if(isBurst(player)){
+            System.out.printf("Player %s burst %n",player.getPlayerName());
+            isGameOver = true;
+        }
+        return false;
+    }
+
+    private static Player theWinnerIs(Player player, Player dealer){
+        if(player.getScore() == 21 && dealer.getScore() == 21){
+            return dealer;
+        }
+        return player.getScore() == 21 ? player : dealer.getScore() == 21 ? dealer : null;
+    }
+
+    private static boolean isBurst(Player player) {
+        return player.score(player.cardValue) > HIGH_SCORE;
+    }
+}
 
--- src/main/java/academy/mindswap/p1g2/casino/server/games/blackjack/Deck.java
+++ src/main/java/academy/mindswap/p1g2/casino/server/games/blackjack/Deck.java
@@ -1,11 +1,10 @@
 package academy.mindswap.p1g2.casino.server.games.blackjack;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class Deck {
-    public List<Card> cards;
+    public List<Integer> cards;
 
     public Deck() {
         cards = new ArrayList<>();
@@ -13,42 +12,24 @@ public class Deck {
 
     /**
      * This method generate a deck of cards to play;
-     * Deck with 48 cards with value of 10 and 4 cards with value of 11 (aces);
      */
-    public void initializeDeck(){
-        int numCards10 = 48;
-        int numCards11 = 4;
-        int cardValue1 = 10;
-        int cardValue2 = 11;
-        for(int i = 0; i < numCards10; i++) {
-            cards.add(new Card(cardValue1));
-        }
-        for(int i = 0; i < numCards11; i++){
-            cards.add(new Card(cardValue2));
-        }
-    }
-
-    /**
-     * Shuffle the cards with the two values that are passed as parameters in th method(initializeDeck());
-     */
-    public void shuffle(){
+    public List generateDeck() {
         Random rd = new Random();
-        for(int i = cards.size() - 1; i > 0; i--){ // iterate over cardsList from de last position (excluding) to the index 1
-            int j = rd.nextInt(i + 1); // generate random number between the number in the list;
-            Card temp = cards.get(i); // uses a temp variable to store the value and trade position with the original;
-            cards.set(i, cards.get(j));
-            cards.set(j, temp);
+        int cardValue = rd.nextInt(13 - 1);
+        for (int i = 0; i < cards.size(); i++) {
+            cards.add(i, cardValue);
         }
+        return cards;
     }
 
-    public Card drawCard() {
+    public int drawCard() {
         if(cards.isEmpty()){
-            return null;
+            return 0;
         }
         return cards.remove(cards.size() - 1);
     }
 
-    public static class Card{
+    private class Card{
         private final int value;
 
         public Card(int value){
@@ -58,7 +39,6 @@ public class Deck {
         private int getValue(){
             return value;
         }
-
 
     }
 
